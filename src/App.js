54c904051694e0f18efa6a85676da7f9bc3840da
Anusha <wagleanusha11@gmail.com>
2022-05-04 17:03:54 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Button from './Button' 
+
+const App = ()=>{
+    return <div>
+    <h1>Useful Website</h1>
+    <h3>Which website do you want to open?</h3 >
+   <Button name="Google" link="https://google.com"/>
+   <Button name="Facebook" link="https://facebook.com"/>
+   <br></br>
+   <Button name="Youtube" link="https://youtube.com"/>
+   <Button name="IPO Result" link="https://iporesult.cdsc.com.np"/>
+   <Button name="Github" link="https://github.com/wagleanusha"/>
+  
+    </div>
+}
+export default App;
